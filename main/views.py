8f71f3c49a45eee135564b2f3ea8a312c0cd303d
Peterials <zjsjinshun@gmail.com>
2017-05-27 20:49:58 +0800

    Add Error Handler Page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
